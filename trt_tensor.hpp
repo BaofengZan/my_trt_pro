5b9dc9a47d3c6986bcd24e0e69947acfddd7c02e
BaofengZan <zanbf1123@163.com>
2022-08-20 08:27:51 +0800

    记录第一阶段的实现思想

--- trt_tensor.hpp
+++ trt_tensor.hpp
@@ -19,9 +19,9 @@ namespace TRT {
         bool resize(const std::initializer_list<int>& dims);
         bool resize(const std::vector<int>& dims);
         void* gpu() { return data_; }
-        int byte_size() { return size_; }
+        int byte_size() { return size_; } // 返回当前Tensor的字节数 
 
-        int size(int index) { return dims_[index]; };
+        int size(int index) { return dims_[index]; };  // 返回指定维度信息
     private:
         void* data_{nullptr};
         int size_{0};
